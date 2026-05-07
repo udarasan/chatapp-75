151bbaa1488ea0b629c5eca417892ae15443074e
Udara San <udarassanjeewa@gmail.com>
2026-05-07 10:34:08 +0530

    Initial commit

---
+++ src/main/java/org/example/chatapp/ClientController.java
@@ -0,0 +1,23 @@
+package org.example.chatapp;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ClientController {
+
+    @FXML
+    private TextArea txtArea;
+
+    @FXML
+    private TextField txtMessage;
+
+    @FXML
+    void sendOnAction(ActionEvent event) {
+
+    }
+
+}
+
